5e781cd013e703ef545344045ec3ea48ea2e0d50
Shubh <shubh2268@gmail.com>
2023-12-30 20:29:29 +0530

    --

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const Detail = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Detail = ({ exerciseDetail }) => {
+
+    const { bodyPart, gifUrl, name, target, equipment, instructions } = exerciseDetail;
+
+    return (
+
+        
+        
+        <div className='flex items-center px-5 bg-slate-900 text-white'>
+            <img src={gifUrl} alt={name} className='w-[300px] md:w-[730px] h-[300px] md:h-[740px]' />
+            <div className='p-10'>
+                <h3 className='text-3xl md:text-6xl font-bold capitalize'>{name}</h3>
+                <p className='py-5 text-xl text-slate-200 italic'>Exercises keep you strong.{' '} <span className='capitalize text-red-600 font-medium'>{name}</span> bup is one
+                    of the best <br /> exercises to target your <span className='capitalize text-red-600 font-medium'>{target}</span>. It will help you improve your{' '}
+                    <br /> mood and gain energy.
+                </p>
+                <ol>
+                    {exerciseDetail.instructions.map((items) => (
+                        <li key={items.id}>{items}</li>
+                    ))}
+                </ol>
+            </div>
+        </div>
+    )
 }
 
 export default Detail;
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Detail from '../components/Detail';
 import SimilarExercises from '../components/SimilarExercises';
 import { fetchData, exerciseOptions } from '../utils/fetchData';
@@ -6,10 +6,39 @@ import { useParams } from 'react-router-dom';
 
 const ExerciseDetail = () => {
 
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const fetchExercisesData = async () => {
+      const exerciseUrl = 'https://exercisedb.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${exerciseUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData(`${exerciseUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equipmentExercisesData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if(!exerciseDetail) {
+    return <div>No Data</div>
+  }
+
   return (
-    <div className='my-14'>
-      <Detail />
-      <SimilarExercises />
+    <div>
+      <Detail exerciseDetail={exerciseDetail} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </div>
   )
 }
