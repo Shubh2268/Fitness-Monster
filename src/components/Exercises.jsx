86f87931f9bbfc381d4ced307f7b15475db85858
Shubh <shubh2268@gmail.com>
2024-03-11 20:07:58 +0530

    --

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -7,7 +7,8 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
-  const [currentPage, setCurrentPage] = useState(1);
+  // const [currentPage, setCurrentPage] = useState(1);
+  const [itemOffset, setItemOffset] = useState(0);
   const [exercisesPerPage] = useState(6);
 
   useEffect(() => {
@@ -15,9 +16,9 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       let exercisesData = [];
 
       if (bodyPart === 'all') {
-        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=200', exerciseOptions);
       } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=200`, exerciseOptions);
       }
 
       setExercises(exercisesData);
@@ -26,15 +27,25 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     fetchExercisesData();
   }, [bodyPart]);
 
-  // Pagination 
-  const indexOfLastExercise = currentPage * exercisesPerPage;
-  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+  // // Pagination 
+  // const indexOfLastExercise = currentPage * exercisesPerPage;
+  // const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  // const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
-  const paginate = (value) => {
-    setCurrentPage(value);
+  const endOffset = itemOffset + exercisesPerPage;
+  const currentExercises = exercises.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(exercises.length / exercisesPerPage);
 
-    window.scrollTo({ top: 100, behavior: 'smooth' });
+  // const paginate = (value) => {
+  //   setCurrentPage(value);
+
+  //   window.scrollTo({ top: 100, behavior: 'smooth' });
+  // };
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * exercisesPerPage) % exercises.length;
+    
+    setItemOffset(newOffset);
   };
 
   if (!currentExercises.length) {
@@ -43,27 +54,27 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   return (
     <div id='exercises' className='my-16 md:my-24'>
-      <div className='flex items-center justify-center my-5'>
+      <div className='flex exercises-center justify-center my-5'>
         <h1 className='font-bold text-2xl md:text-4xl capitalize'>showing results</h1>
       </div>
       <div className='my-1'>
-        <div className='flex flex-wrap items-center justify-center'>
+        <div className='flex flex-wrap exercises-center justify-center'>
           {currentExercises.map((exercise, id) => (
             <ExerciseCard key={id} exercise={exercise} />
           ))}
         </div>
-        <div className='flex items-center justify-center mt-10 md:mt-0'>
-          {/* {exercises && (
+        <div className='flex exercises-center justify-center mt-10 md:mt-0'>
+          {exercises && (
             <ReactPaginate
               breakLabel="..."
-              nextLabel="next >"
-              onPageChange={paginate}
+              nextLabel=">"
+              onPageChange={handlePageClick}
               pageRangeDisplayed={5}
               pageCount={pageCount}
-              previousLabel="< previous"
+              previousLabel="<"
               renderOnZeroPageCount={null}
             />
-          )} */}
+          )}
         </div>
       </div>
     </div>
