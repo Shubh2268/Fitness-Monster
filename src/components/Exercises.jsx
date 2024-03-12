4aec2aa110c7eea2936fcc8b1fda9e7c0bbeff81
Shubh <shubh2268@gmail.com>
2024-03-12 13:48:16 +0530

    --

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -16,9 +16,9 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       let exercisesData = [];
 
       if (bodyPart === 'all') {
-        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=200', exerciseOptions);
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=500', exerciseOptions);
       } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=200`, exerciseOptions);
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=500`, exerciseOptions);
       }
 
       setExercises(exercisesData);
